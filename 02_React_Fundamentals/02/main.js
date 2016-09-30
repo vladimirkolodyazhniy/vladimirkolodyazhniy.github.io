18de27ccd0b3f9694e3ded1f0f8fd9d617774e6a
Vladimir.Kolodyazhny <vkolodyazhniy@acceptic.net>
2016-09-30 10:39:30 +0300

    React_Fundamentals_hw2_2

---
+++ 02_React_Fundamentals/02/main.js
@@ -0,0 +1,55 @@
+const SCALE_FACTOR_MAX = 8,
+      SCALE_FACTOR_STEP = 0.8;
+
+const Clicker = React.createClass({
+    getInitialState() {
+        return {
+            scaleFactor: 1
+        };
+    },
+
+    handleClick() {
+        this.setState({ scaleFactor: this.state.scaleFactor + SCALE_FACTOR_STEP });
+    },
+
+    resetScaleFactor() {
+        this.state.scaleFactor > SCALE_FACTOR_MAX ? this.setState({ scaleFactor: 1 }) : this.state.scaleFactor;
+    },
+
+    componentDidUpdate: function() {
+        this.resetScaleFactor();
+    },
+
+    render() {
+        const { scaleFactor } = this.state;
+
+        const flexContainer = {
+            display: 'flex',
+            alignItems: 'center',
+            height: '100vh',
+            overflow: 'hidden'
+        };
+
+        const flexItem = {
+            backgroundColor: '#000',
+            width: '100px',
+            height: '100px',
+            borderRadius: '50%',
+            margin: 'auto',
+            cursor: 'pointer',
+            transition: 'transform .3s',
+            transform: `scale( ${scaleFactor} )`
+        };
+
+        return (
+            <div style={ flexContainer }>
+                <span style={ flexItem } onClick={ this.handleClick }/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Clicker/>,
+    document.getElementById('root')
+);
